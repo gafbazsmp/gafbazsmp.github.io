4fc2b711e3b45c92059ac8f9bd59005808ec55ec
gafbazsmp <gafolagbazor@gmail.com>
2026-07-09 21:22:27 +0300

    .

--- script.js
+++ script.js
@@ -1,12 +1,11 @@
-// НАСТРОЙКА: Напишите здесь точный IP-адрес вашего сервера (без портов типа :25565)
+// НАСТРОЙКА: Сюда пишем точный IP-адрес сервера
 const SERVER_IP = 'mc.hypixel.net'; 
 
 // 1. Логика копирования IP-адреса
 document.getElementById('ip-block').addEventListener('click', () => {
-    const ipText = document.getElementById('server-ip').innerText;
     const btn = document.getElementById('copy-button');
 
-    navigator.clipboard.writeText(ipText).then(() => {
+    navigator.clipboard.writeText(SERVER_IP).then(() => {
         btn.innerHTML = '<i class="fa-solid fa-check"></i> Скопировано!';
         setTimeout(() => {
             btn.innerHTML = '<i class="fa-regular fa-copy"></i> Копировать IP';
@@ -21,16 +20,13 @@ function updateServerStatus() {
     const onlineText = document.getElementById('online-text');
     const statusDot = document.querySelector('.status-dot');
 
-    // Делаем запрос к бесплатному API статуса майнкрафт-серверов
     fetch(`https://mcsrvstat.us{SERVER_IP}`)
         .then(response => response.json())
         .then(data => {
             if (data.online) {
-                // Если сервер включен
                 statusDot.classList.add('online');
                 onlineText.innerHTML = `В сети: <strong>${data.players.online}</strong> / ${data.players.max}`;
             } else {
-                // Если сервер выключен или IP не существует
                 statusDot.classList.remove('online');
                 onlineText.innerHTML = `<span style="color: #ff4d4d;">Сервер временно оффлайн</span>`;
             }
@@ -41,7 +37,5 @@ function updateServerStatus() {
         });
 }
 
-// Запускаем проверку статуса при загрузке страницы
 updateServerStatus();
-// Автоматически обновляем данные каждые 60 секунд
 setInterval(updateServerStatus, 60000);
